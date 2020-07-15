3e084371d03d9b7e748a94b85da8da89a2777beb
Hakan <user@Users-iMac.local>
2020-07-15 21:41:08 +0200

    title case, join

---
+++ 022_title_case.py
@@ -0,0 +1,6 @@
+def title_case(title, minor_words=''):
+    title = title.capitalize().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join([word if word in minor_words else word.capitalize() for word in title])
+
+
