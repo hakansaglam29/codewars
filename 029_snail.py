cfe50ed4cbfc76db70b87115789d793ddf5ced5b
Hakan <user@Users-iMac.local>
2020-07-18 22:21:32 +0200

    sudoku

--- 029_snail.py
+++ 029_snail.py
@@ -12,27 +12,27 @@ def snail(array):
 
 print(snail(array))
 
-# def snail(array):
-#     dizi= []
-#     n = len(array)
-#     while n>0:
-#         if array==[[]]: break
-#         for i in range(len(array)-1):
-#             if i == 0:
-#                 dizi.extend(array[i])
-#                 array.pop(i)
-#                 n -= 1
-#                 dizi.append(array[i][-1])
-#             else:
-#                 dizi.append(array[i][-1])
-#         for i in range(len(array)-1,-1,-1):
-#             dizi.append(array[-1][i])
-#         array.pop(-1)
-#         n -= 1
-#         for i in range(len(array)-1,-1,-1):
-#             dizi.append(array[i][0])
-#         array2=[]
-#         for i in array:
-#             array2.append(i[1:-1])
-#         array = array2
-#     return dizi
+def snail(array):
+    dizi= []
+    n = len(array)
+    while n>0:
+        if array==[[]]: break
+        for i in range(len(array)-1):
+            if i == 0:
+                dizi.extend(array[i])
+                array.pop(i)
+                n -= 1
+                dizi.append(array[i][-1])
+            else:
+                dizi.append(array[i][-1])
+        for i in range(len(array)-1,-1,-1):
+            dizi.append(array[-1][i])
+        array.pop(-1)
+        n -= 1
+        for i in range(len(array)-1,-1,-1):
+            dizi.append(array[i][0])
+        array2=[]
+        for i in array:
+            array2.append(i[1:-1])
+        array = array2
+    return dizi
