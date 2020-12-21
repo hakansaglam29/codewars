6351bd05ec559d4c6ba743b2d5e4b98e11fd66b4
hakansaglam29 <67585490+hakansaglam29@users.noreply.github.com>
2020-12-21 21:27:38 +0100

    Update 034_sudoku_solver.py

--- 034_sudoku_solver.py
+++ 034_sudoku_solver.py
@@ -1,4 +1,6 @@
-Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
+Write a function that will solve a 9x9 Sudoku puzzle. 
+The function will take one argument consisting of the 2D puzzle array, 
+with the value 0 representing an unknown square.
 
 
 
